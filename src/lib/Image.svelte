715947e6ceb7912c3a5d6cdd67f96e6336a1dc65
Joe Innes <joe@joeinn.es>
2021-11-14 11:21:51 +0100

    Added debouncing and fixes suggested on Discord by Syberag and ronvoluted 🦆

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -1,4 +1,5 @@
-<script>
+<script lang="ts">
+	import { browser } from '$app/env';
 	export let alt;
 	export let aspectRatio = '16/9';
 	export let objectFit = 'cover';
@@ -9,19 +10,51 @@
 	let clientWMax = 0;
 	let domain = '';
 	let imgPath = '';
-	let imgSrc = '';
-	$: {
+	let imgSrc = src;
+
+	let debounce;
+
+	if (browser) {
+		debounce = (callback, wait) => {
+			let timeoutId = null;
+			return (...args) => {
+				window.clearTimeout(timeoutId);
+				timeoutId = window.setTimeout(() => {
+					callback.apply(null, args);
+				}, wait);
+			};
+		};
+	}
+
+	const getNewImgSrc = (clientW) => {
 		try {
-			[, domain, imgPath] = src.match(/\/\/(.*?)\/(.*)/);
+			const imgUrl = new URL(src);
+			domain = imgUrl.host;
+			imgPath = imgUrl.pathname;
 			// If you've already downloaded a higher quality image, then don't bother replacing with a lower quality one
 			clientWMax = Math.max(clientW, clientWMax);
 			// To Do: debounce this in case the user is resizing the window
 			imgSrc = `https://cdn.statically.io/img/${domain}/w=${clientWMax},q=${quality},f=auto/${imgPath}`;
 		} catch (e) {
 			// The user's done something silly like not passing us a full URL to the image, so let's not ask Statically to do anything, just show whatever the user wanted.
-			console.log("Your image could not be optimised by Statically (did you provide a full path and make sure the extension is included?)");
-			imgSrc = src;
+			console.log(
+				'Your image could not be optimised by Statically (did you provide a full path and make sure the extension is included?)'
+			);
+		} finally {
+			return imgSrc;
 		}
+	};
+
+	const getNewImgSrcDB = browser ? debounce(() => getNewImgSrc(clientW), 100) : getNewImgSrc;
+	const handleError = (e) => {
+		console.log(
+			'Your image could not be optimised by Statically (did you provide a full path and make sure the extension is included?)'
+		);
+		e.target.src = src;
+	};
+
+	$: {
+		imgSrc = getNewImgSrcDB(clientW);
 	}
 </script>
 
@@ -31,9 +64,6 @@
 		style="width: 100%; height: 100%; object-fit: {objectFit}"
 		{alt}
 		{loading}
-		on:error={(e) => {
-			console.log("Your image could not be optimised by Statically (did you provide a full path and make sure the extension is included?)");
-			e.target.src = src;
-		}}
+		on:error={handleError}
 	/>
 </div>
